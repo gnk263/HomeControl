b29abaf30b9e71832eb780ebb349ffefba702bab
gnk263 <gnk263@users.noreply.github.com>
2019-08-03 22:46:46 +0900

    IoT ThingsとAPI Gatewayの定義を追加

--- HomeControl/lib/home_control-stack.ts
+++ HomeControl/lib/home_control-stack.ts
@@ -1,9 +1,61 @@
+import * as apigateway from '@aws-cdk/aws-apigateway';
+import * as iot from '@aws-cdk/aws-iot';
+import * as role from '@aws-cdk/aws-iam';
+import { ServicePrincipal, ManagedPolicy } from '@aws-cdk/aws-iam';
 import cdk = require('@aws-cdk/core');
+import { PassthroughBehavior } from '@aws-cdk/aws-apigateway';
 
 export class HomeControlStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+
+    // IoT Things
+    const raspberryPi = new iot.CfnThing(this, 'Raspberry Pi', {
+      thingName: 'HomeControl-RaspberryPi'
+    });
+
+    // Role
+    const apiRole = new role.Role(this, 'ApiRole', {
+      roleName: 'HomeControl-ApiRole',
+      assumedBy: new ServicePrincipal('apigateway.amazonaws.com'),
+      managedPolicies: [
+        ManagedPolicy.fromAwsManagedPolicyName('AWSIoTDataAccess'),
+        ManagedPolicy.fromAwsManagedPolicyName('service-role/AmazonAPIGatewayPushToCloudWatchLogs'),
+      ],
+    });
+
+    // API Gateway
+    const api = new apigateway.RestApi(this, 'HomeControlApi', {
+      restApiName: 'HomeControlApi',
+    });
+
+    const integration = new apigateway.AwsIntegration({
+      service: 'iotdata',
+      subdomain: process.env.IOT_ENDPOINT,
+      integrationHttpMethod: 'POST',
+      path: 'topics/HomeControl/raspberrypi',
+      options: {
+        credentialsRole: apiRole,
+        integrationResponses: [
+          {
+            statusCode: '200',
+          },
+        ],
+        passthroughBehavior: PassthroughBehavior.WHEN_NO_TEMPLATES,
+      },
+    });
+
+    const controlResource = api.root.addResource('control');
+    controlResource.addMethod('POST', integration, {
+      methodResponses: [
+        { statusCode: '200', }
+      ]
+    });
   }
 }
+
+const app = new cdk.App();
+new HomeControlStack(app, 'HomeControlStack');
+app.synth();
